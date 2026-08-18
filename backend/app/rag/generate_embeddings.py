c2c22541517e5323935a89653340be453d937640
Jonathan <jonathanthomasv6@gmail.com>
2026-08-18 01:33:35 -0400

    Refactor generate_embeddings function to use async session for improved performance and scalability

--- backend/app/rag/generate_embeddings.py
+++ backend/app/rag/generate_embeddings.py
@@ -1,6 +1,20 @@
-from ..database import get_database_session
+from ..database import async_session_factory
 from embeddings import embed_text
+from models import Recipe
+from sqlalchemy import Text, cast, or_, select
+from sqlalchemy import Text, cast, or_, select
 
-def generate_embeddings():
-    
+
+limit= 5
+async def generate_embeddings():
+    async with async_session_factory() as session:
+        result = await session.scalars(
+                    select(Recipe)
+                    .order_by(Recipe.id)
+                    .limit(limit)
+                ).all()
+        text=""
+        for recipe in result:
+            text += f"{recipe.title} {recipe.ingredients} {recipe.instructions}"            
+            embed_text(text)
     
