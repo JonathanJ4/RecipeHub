41c549af1475b859037962d98064b8d47991b475
Jonathan <jonathanthomasv6@gmail.com>
2026-08-18 01:43:38 -0400

    Clean up generate_embeddings function by removing unnecessary text initialization and adding newlines for better readability

--- backend/app/rag/generate_embeddings.py
+++ backend/app/rag/generate_embeddings.py
@@ -2,7 +2,12 @@ from .embeddings import embed_text
 from ..database import async_session_factory
 from ..models.recipe import Recipe
 from sqlalchemy import select
+import asyncio
 
+
+
+    
+    
 limit= 5
 async def generate_embeddings():
     async with async_session_factory() as session:
@@ -14,10 +19,11 @@ async def generate_embeddings():
         recipes= result.all()
         
         for recipe in recipes:
-            text=""
-            text += f"{recipe.title} {recipe.ingredients} {recipe.instructions}"            
+            text = f"{recipe.title} {recipe.ingredients} {recipe.instructions}"            
             recipe.embedding = embed_text(text)
             
         
         await session.commit()
-    
+
+if __name__ == "__main__":
+    asyncio.run(generate_embeddings())
