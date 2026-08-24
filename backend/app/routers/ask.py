c2e68470eebb52d8b889183c1d77ff133d3bdadb
Jonathan <jonathanthomasv6@gmail.com>
2026-08-24 01:07:52 -0400

    Refactor ask router to use correct response model and update endpoint prefix Add Ask page component

--- backend/app/routers/ask.py
+++ backend/app/routers/ask.py
@@ -3,9 +3,9 @@ from rag.generation import generation
 from ..schemas.ask import AskRequest,AskResponse
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/recipes", tags=["Recipes"])
+router = APIRouter(tags=["Ask"])
 
-@router.post("/ask", response_model=[AskResponse()])
+@router.post("/ask", response_model=[AskResponse])
 async def ask(request:AskRequest):
     retrieved_recipes = await retrieval(request.query)
     response = await generation(request.query, retrieved_recipes)
